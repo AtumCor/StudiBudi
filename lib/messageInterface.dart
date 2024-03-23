e4657ddfe516399d5f8eb549a4f001310e312859
Jiaming Meng <49968036+AtumCor@users.noreply.github.com>
2024-03-23 19:30:36 -0400

    added message interface

--- lib/messageInterface.dart
+++ lib/messageInterface.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 import 'user_data.dart';
 
-class UserProfileScreen extends StatelessWidget {
+class UserProfileScreen extends StatefulWidget {
   final User user;
 
   UserProfileScreen({required this.user});
 
   @override
+  _UserProfileScreenState createState() => _UserProfileScreenState();
+}
+
+final TextEditingController controller = TextEditingController();
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  String message = 'Type your message...';
+  String message1 = '';
+  String message2 = '';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(user.username),
+        title: Text(widget.user.username),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -18,7 +29,7 @@ class UserProfileScreen extends StatelessWidget {
         children: [
           CircleAvatar(
             radius: 50,
-            backgroundImage: NetworkImage(user.pictureUrl),
+            backgroundImage: NetworkImage(widget.user.pictureUrl),
           ),
           SizedBox(height: 16),
           Divider(thickness: 2),
@@ -26,9 +37,66 @@ class UserProfileScreen extends StatelessWidget {
           Expanded(
             child: Container(
               padding: EdgeInsets.all(16),
-              child: Text(
-                'Chatbox space',
-                style: TextStyle(fontSize: 18),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          message1,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          message2,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  TextField(
+                    controller: controller,
+                    onChanged: (value) {
+                      setState(() {
+                        message2 = value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      hintText: message,
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        message1 = message2;
+                        message2 = message;
+                        controller.clear();
+                      });
+                    },
+                    child: Text('Send'),
+                  ),
+                ],
               ),
             ),
           ),
