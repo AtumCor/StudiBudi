bc6289e0f912e24fc31d2f629f1aa398c767fab1
Jiaming Meng <49968036+AtumCor@users.noreply.github.com>
2024-03-23 15:28:55 -0400

    add phone numbers

--- lib/user_data.dart
+++ lib/user_data.dart
@@ -6,6 +6,7 @@ class User {
   final String location;
   final String school;
   final List<String> classes;
+  final String phoneNumber; // New field for phone number
 
   User({
     required this.username,
@@ -13,6 +14,7 @@ class User {
     required this.location,
     required this.school,
     required this.classes,
+    required this.phoneNumber, // Update constructor
   });
 }
 
@@ -27,6 +29,7 @@ class UserData {
         location: 'City${i % 5}', // Cycle through 5 cities
         school: 'School${(i % 3) + 1}', // Cycle through 3 schools
         classes: ['Class${(i % 4) + 1}', 'Class${((i + 1) % 4) + 1}'], // Each user with 2 classes, cycling through 4 classes
+        phoneNumber: '+1234567${(8000 + i).toString().padLeft(4, '0')}', // Generating phone numbers starting from +12345678001
       ),
   ];
 }
